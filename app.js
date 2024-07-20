59f68449a2311a826b10934b6b7f95c8e94859ad
narsis <narsiskh.2013@gmail.com>
2024-07-20 20:19:21 +0330

    Learning about npm.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const http = require('http');
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-    res.write('Welcome to our home page')
-    res.end()
-})
+// local dependency - use it only in this particular
+// npm i <packageName>
 
-server.listen(5000)
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root create properties etc.)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
