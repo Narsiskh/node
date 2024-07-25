d0ecbe55f2c9d12484854feb53bff1c98723c6df
narsis <narsiskh.2013@gmail.com>
2024-07-25 17:32:42 +0330

    Learning about call stack

--- 15-call-stsck.js
+++ 15-call-stsck.js
@@ -4,12 +4,13 @@ const baz = () => console.log('baz')
 
 const foo = () => {
     console.log('foo')
-    bar()
+    //bar()
+    setTimeout(bar, 1000)
     baz()
 }
 
 foo()
 // foo
-// bar
 // baz
-// Call Stack will put this function in a row or a que and then executes it one by one
+// bar
+// Call Stack will first execute the `foo` function, inside `foo` it will execute the `setTimeOut` function. As you know this `setTimeOut` function have a timer, so it will execute after 1 second. Just after that the call stack will move to the next function and execute it, and at the end the call stack will execute the function the is causing delay .
